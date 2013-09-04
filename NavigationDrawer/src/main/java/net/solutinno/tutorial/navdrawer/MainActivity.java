cdae5f23457dd5adb170ab44405d561d46ab850e
Bálint Böde <balint.bode@gmail.com>
2013-09-04 13:54:09 +0200

    Implement tutorial

--- NavigationDrawer/src/main/java/net/solutinno/tutorial/navdrawer/MainActivity.java
+++ NavigationDrawer/src/main/java/net/solutinno/tutorial/navdrawer/MainActivity.java
@@ -1,15 +1,45 @@
 package net.solutinno.tutorial.navdrawer;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private String[] menuItems = new String[] { "Item1", "Item2", "Item3", "Item4", "Item5" };
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mListView;
+    private TextView mContentText;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mListView = (ListView) findViewById(R.id.left_drawer);
+        mContentText = (TextView) findViewById(R.id.content_text);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mListView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.item_title, menuItems));
+        mListView.setOnItemClickListener(mOnItemClickListener);
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
     }
 
     @Override
@@ -17,5 +47,34 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            TextView v = (TextView) view.findViewById(R.id.item_title);
+            mContentText.setText(v.getText());
+            mDrawerLayout.closeDrawer(mListView);
+        }
+    };
+
 }
